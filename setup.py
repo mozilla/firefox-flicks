1a1b3fee386284c67e3521e1f2e5b6b91dceb874
Michael Kelly <mkelly@mozilla.com>
2012-01-10 14:13:37 -0800

    Renamed project module to flicks

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 
-setup(name='project',
+setup(name='flicks',
       version='1.0',
       description='Django application.',
       long_description='',
