01a4a92f69219e081171aa1ad9c0215efec8f69d
Michael Kelly <mkelly@mozilla.com>
2012-02-03 11:33:46 -0800

    Remove test settings to see if it fixes jenkins.

--- flicks/settings/__init__.py
+++ flicks/settings/__init__.py
@@ -5,10 +5,11 @@ except ImportError, exc:
     exc.args = tuple(['%s (did you rename settings/local.py-dist?)'
                       % exc.args[0]])
     raise exc
-
+"""
 import sys
 if sys.argv[1] == 'test':
     try:
         from .test import *
     except ImportError:
         pass
+"""
