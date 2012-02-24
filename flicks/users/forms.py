d7df29f9db455f2db3e72c9b0b2ffa124b2cf62d
Jen Fong-Adwent <jen@ednapiranha.com>
2012-02-24 18:00:30 -0500

    added agreement checkbox to signup; fix bug 730440

--- flicks/users/forms.py
+++ flicks/users/forms.py
@@ -1,10 +1,27 @@
-from happyforms import ModelForm
+from django import forms
+
+import jinja2
+from happyforms import Form, ModelForm
+from tower import ugettext as _, ugettext_lazy as _lazy
 
 from flicks.users.models import UserProfile
 
 
-class UserProfileForm(ModelForm):
-    """Video upload form."""
+class UserProfileEditForm(ModelForm):
+    """User edit form."""
     class Meta:
         model = UserProfile
         exclude = ('user',)
+
+
+class UserProfileCreateForm(UserProfileEditForm):
+    """User create form."""
+    agreement = forms.BooleanField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileCreateForm, self).__init__(*args, **kwargs)
+
+        self.fields['agreement'].label = jinja2.Markup(_(
+            u"I'm okay with Mozilla handling this info as you explain in your "
+            "<a href='{url}' target='_blank'>privacy policy</a>.")).format(
+        	url='http://www.mozilla.org/en-US/privacy-policy.html')
