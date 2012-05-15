65c83537cf12f9dd42fbad2bc6466330191d6345
Michael Kelly <mkelly@mozilla.com>
2012-05-15 11:10:52 -0700

    Moar review fixes and a CSS fix for the footer.

--- media/js/winners.js
+++ media/js/winners.js
@@ -16,31 +16,31 @@
             didScroll = false;
             var scrollTop = $window.scrollTop();
 
-            if(scrollTop >= navTop.top) {
-                if(!fixed) {
+            if (scrollTop >= navTop.top) {
+                if (!fixed) {
                     fixed = true;
                     $nav.addClass("fixed");
-                    $content.css({ paddingTop: navHeight+24 });
+                    $content.css({ 'padding-top': navHeight + 24 });
                 }
             } else {
-                if(fixed) {
+                if (fixed) {
                     fixed = false;
                     $nav.removeClass("fixed");
-                    $content.css({ paddingTop: "0" });
+                    $content.css({ 'padding-top': '0' });
                 }
             }
         }
     };
 
-    // Check for an adjusted scrollbar every 300ms.
-    setInterval(adjustScrollbar, 300);
+    // Check for an adjusted scrollbar every 250ms.
+    setInterval(adjustScrollbar, 250);
 
     // Bind scrolling to linked element.
-    $window.on('click', '#nav-sections a', function(e){
+    $window.on('click', '#nav-sections a', function(e) {
         e.preventDefault();
 
         // Extract the target element's ID from the link's href.
-        var target = $(this).attr("href").replace( /.*?(#.*)/g, "$1" );
+        var target = $(this).attr("href").hash;
         $('html, body').animate({
             scrollTop: $(target).offset().top-navHeight
         }, 1000);
