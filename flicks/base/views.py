665df5081893f47245bdbdf8190e5dc6949a0b8f
Michael Kelly <mkelly@mozilla.com>
2013-01-22 17:22:25 -0500

    Localize Creative Commons Wikipedia link.

--- flicks/base/views.py
+++ flicks/base/views.py
@@ -4,6 +4,21 @@ from django.shortcuts import render
 from flicks.base.util import absolutify
 
 
+LINK_PDWIKI = {
+    'en-US': 'https://en.wikipedia.org/wiki/Public_domain',
+    'de': 'https://de.wikipedia.org/wiki/Gemeinfreiheit',
+    'fr': 'https://fr.wikipedia.org/wiki/Domaine_public_%28propri%C3%A9t%C3%A9_intellectuelle%29',
+    'es': 'https://es.wikipedia.org/wiki/Dominio_p%C3%BAblico',
+    'nl': 'https://nl.wikipedia.org/wiki/Publiek_domein',
+    'pl': 'https://pl.wikipedia.org/wiki/Domena_publiczna',
+    'pt-BR': 'https://pt.wikipedia.org/wiki/Dom%C3%ADnio_p%C3%BAblico',
+    'sl': 'https://sl.wikipedia.org/wiki/Javna_last',
+    'sq': 'https://sq.wikipedia.org/wiki/Domen_publik',
+    'zh-CN': 'https://zh.wikipedia.org/wiki/%E5%85%AC%E6%9C%89%E9%A2%86%E5%9F%9F ',
+    'zh-TW': 'https://zh.wikipedia.org/wiki/%E5%85%AC%E6%9C%89%E9%A2%86%E5%9F%9F ',
+}
+
+
 def home(request):
     """Home page."""
     return render(request, 'home.html')
@@ -39,7 +54,9 @@ def partners(request):
 
 def faq(request):
     """FAQ page."""
-    return render(request, 'faq.html')
+    return render(request, 'faq.html', {
+        'link_pdwiki': LINK_PDWIKI.get(request.locale, LINK_PDWIKI['en-US'])
+    })
 
 
 def rules(request):
