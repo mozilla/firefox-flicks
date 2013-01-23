2eb927d09c221265ee871d24887e2d2631b1045d
Michael Kelly <mkelly@mozilla.com>
2013-01-23 15:31:47 -0500

    Add sq to PROD_LANGUUAGES.

--- flicks/settings/base.py
+++ flicks/settings/base.py
@@ -38,7 +38,8 @@ MINIFY_BUNDLES = {
     }
 }
 
-PROD_LANGUAGES = ('de', 'en-US', 'es', 'fr', 'nl', 'pl', 'pt-BR', 'sl', 'zh-TW')
+PROD_LANGUAGES = ('de', 'en-US', 'es', 'fr', 'nl', 'pl', 'pt-BR', 'sl', 'sq',
+                  'zh-TW')
 
 # Defines the views served for root URLs.
 ROOT_URLCONF = 'flicks.urls'
