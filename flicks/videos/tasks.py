c62b2a5aae45ca2756d63d7a36cd530089b984d1
Michael Kelly <mkelly@mozilla.com>
2013-02-06 17:28:04 -0500

    Update processing task to retry later if it fails.

--- flicks/videos/tasks.py
+++ flicks/videos/tasks.py
@@ -7,27 +7,31 @@ from flicks.videos import vimeo
 from flicks.videos.models import Video
 
 
-@task
+@task(default_retry_delay=30 * 60)  # Retry once every 30 minutes.
 def process_video(video_id):
     """Update metadata about the given video on Vimeo."""
     video = get_object_or_none(Video, id=video_id)
     if video:
-        vimeo.setTitle(video.vimeo_id, video.title)
+        try:
+            vimeo.setTitle(video.vimeo_id, video.title)
 
-        # Set description to title + author + description.
-        description = '{title} by {author}\n\n{description}'.format(
-            title=video.title, author=video.user.profile.display_name,
-            description=video.description)
-        vimeo.setDescription(video.vimeo_id, description)
+            # Set description to title + author + description.
+            description = '{title} by {author}\n\n{description}'.format(
+                title=video.title, author=video.user.profile.display_name,
+                description=video.description)
+            vimeo.setDescription(video.vimeo_id, description)
 
-        # Add to the channel for the user's region.
-        channels = settings.VIMEO_REGION_CHANNELS
-        channel_id = channels.get(video.user.profile.region, None)
-        if channel_id:
-            vimeo.addToChannel(video.vimeo_id, channel_id)
+            # Add to the channel for the user's region.
+            channels = settings.VIMEO_REGION_CHANNELS
+            channel_id = channels.get(video.user.profile.region, None)
+            if channel_id:
+                vimeo.addToChannel(video.vimeo_id, channel_id)
 
-        # Set their country code as a tag.
-        vimeo.addTags(video.vimeo_id, [video.user.profile.country])
+            # Set their country code as a tag.
+            vimeo.addTags(video.vimeo_id, [video.user.profile.country])
 
-        video.processed = True
-        video.save()
+            video.processed = True
+            video.save()
+        except vimeo.VimeoServiceError, e:
+            # If Vimeo is down, retry later.
+            raise process_video.retry(exc=e)
--- flicks/videos/tests/test_tasks.py
+++ flicks/videos/tests/test_tasks.py
@@ -4,21 +4,22 @@ from nose.tools import ok_
 from flicks.base import regions
 from flicks.base.tests import TestCase
 from flicks.users.tests import UserProfileFactory
+from flicks.videos import vimeo
 from flicks.videos.tasks import process_video
 from flicks.videos.tests import VideoFactory
 
 
 @patch('flicks.videos.tasks.vimeo')
 class TestProcessVideo(TestCase):
-    def test_no_video(self, vimeo):
+    def test_no_video(self, mock_vimeo):
         """If no video is found with the given id, do nothing."""
         process_video(1387589156831)
-        ok_(not vimeo.setTitle.called)
-        ok_(not vimeo.setDescription.called)
-        ok_(not vimeo.addToChannel.called)
-        ok_(not vimeo.addTags.called)
+        ok_(not mock_vimeo.setTitle.called)
+        ok_(not mock_vimeo.setDescription.called)
+        ok_(not mock_vimeo.addToChannel.called)
+        ok_(not mock_vimeo.addTags.called)
 
-    def test_valid_video(self, vimeo):
+    def test_valid_video(self, mock_vimeo):
         """If a valid video id is given, update the Vimeo metadata."""
         user = UserProfileFactory.create(nickname='name', country='us').user
         video = VideoFactory.create(user=user, title='blah', description='asdf',
@@ -27,7 +28,23 @@ class TestProcessVideo(TestCase):
         with self.settings(VIMEO_REGION_CHANNELS={regions.NORTH_AMERICA: 18}):
             process_video(video.id)
 
-        vimeo.setTitle.assert_called_with(7, 'blah')
-        vimeo.setDescription.assert_called_with(7, 'blah by name\n\nasdf')
-        vimeo.addToChannel.assert_called_with(7, 18)
-        vimeo.addTags.assert_called_with(7, ['us'])
+        mock_vimeo.setTitle.assert_called_with(7, 'blah')
+        mock_vimeo.setDescription.assert_called_with(7, 'blah by name\n\nasdf')
+        mock_vimeo.addToChannel.assert_called_with(7, 18)
+        mock_vimeo.addTags.assert_called_with(7, ['us'])
+
+    @patch('flicks.videos.tasks.process_video.retry', wraps=process_video.retry)
+    def test_retry(self, retry, mock_vimeo):
+        """
+        If a vimeo command fails due to a VimeoServiceError, raise a retry
+        exception for celery to retry later.
+        """
+        mock_vimeo.setTitle.side_effect = vimeo.VimeoServiceError
+        mock_vimeo.VimeoServiceError = vimeo.VimeoServiceError
+        video = VideoFactory.create()
+        ok_(not retry.called)
+
+        # When not being executed in a worker, retry just raises the exception.
+        with self.assertRaises(vimeo.VimeoServiceError):
+            process_video(video.id)
+        ok_(retry.called)
