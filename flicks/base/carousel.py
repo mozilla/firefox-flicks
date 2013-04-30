af4573347c2081ced2274fd1ef97f258e77b98ff
Michael Kelly <mkelly@mozilla.com>
2013-04-30 14:29:42 -0400

    Use dates instead of datetimes for carousel logic.

--- flicks/base/carousel.py
+++ flicks/base/carousel.py
@@ -1,31 +1,31 @@
 from collections import namedtuple
-from datetime import datetime
+from datetime import date
 
 
 ScheduleItem = namedtuple('ScheduleItem', ['start', 'end', 'slides'])
 slide_schedule = (
-    # EX: ScheduleItem(datetime(2013, 4, 5), datetime(2013, 4, 22), ['slide-two.html', 'slide-three.html']),
-    ScheduleItem(datetime(2013, 4, 9), datetime(2013, 4, 21), ['blogpost_judges.html', 'blogpost_makeyourflickawinner.html', 'blogpost_getcreative.html']),
-    ScheduleItem(datetime(2013, 4, 22), datetime(2013, 4, 30), ['blogpost_baficiwinner.html', 'blogpost_schwartzberg.html', 'blogpost_makeyourflickawinner.html']),
+    # EX: ScheduleItem(date(2013, 4, 5), date(2013, 4, 22), ['slide-two.html', 'slide-three.html']),
+    ScheduleItem(date(2013, 4, 9), date(2013, 4, 21), ['blogpost_judges.html', 'blogpost_makeyourflickawinner.html', 'blogpost_getcreative.html']),
+    ScheduleItem(date(2013, 4, 22), date(2013, 4, 30), ['blogpost_baficiwinner.html', 'blogpost_schwartzberg.html', 'blogpost_makeyourflickawinner.html']),
 )
 
 
-def get_slides(display_datetime=None):
+def get_slides(display_date=None):
     """
     Generate a list of the template paths to the slides that should be displayed
-    on the homepage based on the given datetime.
+    on the homepage based on the given date.
 
-    :param display_datetime:
-        Datetime to use to determine which promos to display. Defaults to
-        datetime.now().
+    :param display_date:
+        Date to use to determine which promos to display. Defaults to
+        date.now().
     """
-    display_datetime = display_datetime or datetime.now()
+    display_date = display_date or date.today()
     return ('carousel/{0}'.format(name) for name in
-            _get_slide_names(display_datetime))
+            _get_slide_names(display_date))
 
 
-def _get_slide_names(display_datetime):
+def _get_slide_names(display_date):
     for item in slide_schedule:
-        if item.start <= display_datetime <= item.end:
+        if item.start <= display_date <= item.end:
             return item.slides
     return ['slide-two.html', 'slide-three.html']  # Default
--- flicks/base/tests/test_carousel.py
+++ flicks/base/tests/test_carousel.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 
 from mock import patch
 from nose.tools import eq_
@@ -8,29 +8,29 @@ from flicks.base.tests import TestCase
 
 
 mock_schedule = (
-    ScheduleItem(datetime(2013, 4, 5), datetime(2013, 4, 22), ['test1.html', 'test2.html']),
-    ScheduleItem(datetime(2013, 4, 26), datetime(2013, 4, 28), ['test3.html', 'test4.html']),
-    ScheduleItem(datetime(2013, 4, 29), datetime(2013, 5, 2), ['test5.html', 'test6.html']),
+    ScheduleItem(date(2013, 4, 5), date(2013, 4, 22), ['test1.html', 'test2.html']),
+    ScheduleItem(date(2013, 4, 26), date(2013, 4, 28), ['test3.html', 'test4.html']),
+    ScheduleItem(date(2013, 4, 29), date(2013, 5, 2), ['test5.html', 'test6.html']),
 )
 
 
 @patch('flicks.base.carousel.slide_schedule', mock_schedule)
 class GetSlidesTests(TestCase):
-    @patch('flicks.base.carousel.datetime')
-    def test_now(self, dt):
+    @patch('flicks.base.carousel.date')
+    def test_now(self, mock_date):
         """
         If a date isn't passed to get_slides, it should return slides for the
         current date.
         """
-        dt.now.return_value = datetime(2013, 4, 8)
+        mock_date.today.return_value = date(2013, 4, 8)
         eq_(list(get_slides()), ['carousel/test1.html', 'carousel/test2.html'])
 
-    def test_display_datetime(self):
+    def test_display_date(self):
         """
         If a date is passed to get_slides, it should return slides for that date
         instead of the current date.
         """
-        eq_(list(get_slides(datetime(2013, 5, 1))),
+        eq_(list(get_slides(date(2013, 5, 1))),
             ['carousel/test5.html', 'carousel/test6.html'])
 
     def test_default(self):
@@ -38,5 +38,5 @@ class GetSlidesTests(TestCase):
         If the date given to get_slides doesn't match any of the configured
         dates, it should return the default slides.
         """
-        eq_(list(get_slides(datetime(2012, 2, 2))),
+        eq_(list(get_slides(date(2012, 2, 2))),
             ['carousel/slide-two.html', 'carousel/slide-three.html'])
