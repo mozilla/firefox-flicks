4c59bdec000b86aed1fac990838a5b74bba00b68
Benjamin Sternthal <ben@devpatch.com>
2013-05-06 14:15:50 -0700

    Fix Bug 854725: Added promos for May 8 & May 18.
    Added additional promos through June 1
    
    i am a schmuck and forgot to remove my comments
    
    ugh i need help

--- flicks/base/carousel.py
+++ flicks/base/carousel.py
@@ -7,7 +7,11 @@ slide_schedule = (
     # EX: ScheduleItem(date(2013, 4, 5), date(2013, 4, 22), ['slide-two.html', 'slide-three.html']),
     ScheduleItem(date(2013, 4, 9), date(2013, 4, 21), ['blogpost_judges.html', 'blogpost_makeyourflickawinner.html', 'blogpost_getcreative.html']),
     ScheduleItem(date(2013, 4, 22), date(2013, 4, 30), ['blogpost_baficiwinner.html', 'blogpost_schwartzberg.html', 'blogpost_makeyourflickawinner.html']),
-    ScheduleItem(date(2013, 5, 1), date(2013, 5, 8), ['blogpost_shootingpeople.html', 'blogpost_baficiwinner.html', 'blogpost_makeyourflickawinner.html']),
+    ScheduleItem(date(2013, 5, 1), date(2013, 5, 7), ['blogpost_shootingpeople.html', 'blogpost_baficiwinner.html', 'blogpost_makeyourflickawinner.html']),
+    ScheduleItem(date(2013, 5, 8), date(2013, 5, 17), ['may29_earlyentry.html', 'blogpost_shootingpeople.html', 'blogpost_makeyourflickawinner.html']),
+    ScheduleItem(date(2013, 5, 18), date(2013, 5, 29), ['may29_earlyentry.html', 'blogpost_getmobilized.html', 'blogpost_makeyourflickawinner.html']),
+    ScheduleItem(date(2013, 5, 30), date(2013, 5, 31), ['blogpost_winbig.html', 'blogpost_getmobilized.html', 'blogpost_makeyourflickawinner.html']),
+    ScheduleItem(date(2013, 6, 1), date(2013, 6, 11), ['july3_earlyentry.html', 'blogpost_getmobilized.html', 'july31_earlyentry.html']),
 )
 
 
