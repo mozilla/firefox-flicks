b70702d92d42baae54ba3f9e01c337dc71273d63
Michael Kelly <mkelly@mozilla.com>
2013-08-01 15:41:33 -0400

    Add nickname and username to admin to help diagnose some issues.

--- flicks/users/admin.py
+++ flicks/users/admin.py
@@ -5,11 +5,16 @@ from django.contrib.auth.models import User
 
 class UserAdmin(admin.UserAdmin):
     """Configuration for the user admin pages."""
-    list_display = ['email', 'full_name', 'country', 'is_staff']
-    search_fields = ['email', 'userprofile__full_name', 'userprofile__nickname']
+    list_display = ['email', 'nickname', 'username', 'full_name', 'country',
+                    'is_staff']
+    search_fields = ['email', 'userprofile__nickname', 'username',
+                     'userprofile__full_name', 'userprofile__nickname']
 
     def full_name(self, user):
         return user.profile.full_name
+
+    def nickname(self, user):
+        return user.profile.nickname
 
     def country(self, user):
         return user.profile.country
